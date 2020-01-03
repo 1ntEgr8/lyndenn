1268869ba26cc148be76bb6430dd66ada4e7e3b5
Elton <eltonp3103@gmail.com>
2020-01-03 11:50:05 -0500

    setup for demo is done

--- demos/index.js
+++ demos/index.js
@@ -1,58 +1,70 @@
-const dragon = lyndenn({
-    variables: ["X", "Y"],
-    constants: ["F", "+", "-"],
-    axiom: "FX",
-    rules: {
-        "X": "X+YF+",
-        "Y": "-FX-Y"
-    },
-    ops: {
-        "X": "",
-        "Y": "",
-        "F": "f10",
-        "+": "r-90",
-        "-": "r90"
-    }
-});
+function prepCanvas(turtle) {
+    document.getElementById("canvas").appendChild(turtle.canvas);
+    centerTurtle(turtle);
+    turtle.setStyles({
+        "stroke-width": "4px"
+    })
+}
 
-const gosper = lyndenn({
-    variables: ["A", "B"],
-    constants: ["-", "+"],
-    axiom: "A",
-    rules: {
-        "A": "A-B--B+A++AA+B-",
-        "B": "+A-BB--B-A++A+B"
-    },
-    ops: {
-        "A": "f10",
-        "B": "f10",
-        "-": "r60",
-        "+": "r-60"
-    }
-});
+function centerTurtle(turtle) {
+    turtle.moveTo(350, 250);
+}
 
-const tree = lyndenn({
-    variables: ["0", "1"],
-    constants: ["[", "]"],
-    axiom: "0",
-    rules: {
-        "1": "11",
-        "0": "1[0]0"
-    },
-    ops: {
-        "0": "f0.5",
-        "1": "f0.5",
-        "[": "> r-45",
-        "]": "< r45"
-    }
-});
+function dragon() {
+    const dragon = lyndenn({
+        variables: ["X", "Y"],
+        constants: ["F", "+", "-"],
+        axiom: "FX",
+        rules: {
+            "X": "X+YF+",
+            "Y": "-FX-Y"
+        },
+        ops: {
+            "X": "",
+            "Y": "",
+            "F": "f10",
+            "+": "r-90",
+            "-": "r90"
+        }
+    });
+}
 
-// gosper.turtle.moveTo(400, 400);
-// gosper.draw(4);
+function gosper() {
+    const gosper = lyndenn({
+        variables: ["A", "B"],
+        constants: ["-", "+"],
+        axiom: "A",
+        rules: {
+            "A": "A-B--B+A++AA+B-",
+            "B": "+A-BB--B-A++A+B"
+        },
+        ops: {
+            "A": "f10",
+            "B": "f10",
+            "-": "r60",
+            "+": "r-60"
+        }
+    });
+    prepCanvas(gosper.turtle);
+    gosper.draw(3);
+}
 
-// tree.turtle.moveTo(500, 500);
-// tree.turtle.setHeading(-90);
-// tree.draw(10)
+function tree() {
+    const tree = lyndenn({
+        variables: ["0", "1"],
+        constants: ["[", "]"],
+        axiom: "0",
+        rules: {
+            "1": "11",
+            "0": "1[0]0"
+        },
+        ops: {
+            "0": "f0.5",
+            "1": "f0.5",
+            "[": "> r-45",
+            "]": "< r45"
+        }
+    });
+}
 
-dragon.turtle.moveTo(300, 300);
-dragon.draw(10);
+gosper();
